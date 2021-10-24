823b9bbcb762f578655dc5a335c4727f3e9d45ed
Angelos Koulouris <cool.lab.gr@gmail.com>
2021-10-24 16:30:28 +0300

    fresh svelte install with vite

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+const config = {
+  mode: "jit",
+  purge: {
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    options: {
+      safelist: [
+        /data-theme$/,
+      ]
+    },
+  },
+  
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [ require('daisyui') ],
+};
+
+module.exports = config;
